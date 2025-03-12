708315bb032d4acc7c3eb77cd2a6f97e3ffebaf0
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-03-12 16:41:48 +0000

    feat: Added methods of handler user

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"tr-search-back/internal/domain/user"
 	"tr-search-back/internal/usecases"
@@ -10,11 +11,14 @@ import (
 )
 
 type UserHandler struct {
-	createUserUseCase *usecases.UseCase
+	createUserUseCase   *usecases.UseCase
+	deleteUserUseCase   *usecases.DeleteUserUseCase
+	updateUserUseCase   *usecases.UpdateUserUseCase
+	getUserEmailUseCase *usecases.GetUserEmailUseCase
 }
 
-func NewUserHandler(createUserUseCase *usecases.UseCase) *UserHandler {
-	return &UserHandler{createUserUseCase: createUserUseCase}
+func NewUserHandler(createUserUseCase *usecases.UseCase, deleteUserUseCase *usecases.DeleteUserUseCase, updateUserUseCase *usecases.UpdateUserUseCase, getUserEmailUseCase *usecases.GetUserEmailUseCase) *UserHandler {
+	return &UserHandler{createUserUseCase: createUserUseCase, deleteUserUseCase: deleteUserUseCase, updateUserUseCase: updateUserUseCase, getUserEmailUseCase: getUserEmailUseCase}
 }
 
 func (h *UserHandler) CreateUser(c *gin.Context) {
@@ -30,4 +34,52 @@ func (h *UserHandler) CreateUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, u)
+}
+
+func (h *UserHandler) UpdateUser(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	var u user.User
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	u.ID = uint(id)
+
+	if err := h.updateUserUseCase.Execute(&u); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, u)
+}
+
+func (h *UserHandler) GetUserEmail(c *gin.Context) {
+	email := c.Param("email")
+	u, err := h.getUserEmailUseCase.Execute(email)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, u)
+}
+
+func (h *UserHandler) DeleteUser(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	if err := h.deleteUserUseCase.Execute(uint(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
 }
